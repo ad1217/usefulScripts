644b9d19598480378430c5cc40666f14cd1ad90c
Adam Goldsmith <adamgoldsmith1217@gmail.com>
2014-08-10 23:28:33 -0400

    better support for multiple interfaces

--- netstatus.sh
+++ netstatus.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-ip=$(ip -4 a show $1|grep -o -P '(?<=inet )[0-9.]*')
+interfaces=$(echo "$@" | sed 's/ /\\|/g')
+ip=$(ip -4 a|grep -o -P '((?<=inet )[0-9.]*)|(^\d: \K[^:]*)'|tr '\n' ':'|grep -oP '(:|^)\K[^:]*:\d[^:$]*'|grep -v lo|grep "$interfaces"|tr '\n' ' ')
 GPing=$(ping -c1 -W1 8.8.8.8|(grep "bytes from"||echo "Down")|sed 's/.*time=\([0-9\.]*\) ms.*/\1ms/')
-echo -n "$ip G:$GPing"
+echo -n "${ip}G:$GPing"
 
