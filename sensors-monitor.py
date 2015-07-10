770e362b961960df2fa2597d32ce2af729b33ff6
Adam Goldsmith <adam@adamgoldsmith.name>
2015-07-10 15:18:00 -0400

    sensors-monitor: clean up sensors color code

--- sensors-monitor.py
+++ sensors-monitor.py
@@ -3,7 +3,8 @@ import sensors
 import subprocess
 from glob import glob
 
-sensorsToShow = {'Physical id 0': ["C", 70,100], 'Left side  ': ["RPM", 3000,5000]}
+sensorsToShow = {'Physical id 0': ["C", 70,100],
+                 'Left side  '  : ["RPM", 3000,5000]}
 
 cpuGovernors = { "powersave" :  '<span fgcolor="green">s</span>',
                  "performance": '<span fgcolor="red">p</span>',
@@ -16,8 +17,11 @@ try:
     for chip in sensors.iter_detected_chips():
         for feature in chip:
             if feature.label in sensorsToShow:
-                out += '<span fgcolor="%s">%.f%s</span> ' % ("#00B000" if feature.get_value() < sensorsToShow[feature.label][1] else ("yellow" if feature.get_value() < sensorsToShow[feature.label][2] else "red"), feature.get_value(), sensorsToShow[feature.label][0])
-    out += '<span>%.2f%s</span>' % (float(max(subprocess.check_output("cat /proc/cpuinfo | grep MHz | grep -o '[0-9]*'", shell=True).split()))/1000, "GHz")
+                color = ("#00B000" if feature.get_value() < sensorsToShow[feature.label][1]
+                         else ("yellow" if feature.get_value() < sensorsToShow[feature.label][2]
+                               else "red"))
+                out += '<span fgcolor="%s">%.f%s</span> ' % (color, feature.get_value(), sensorsToShow[feature.label][0])
+    out += '<span>%.2f%s</span>' % (float(max(subprocess.check_output("grep MHz /proc/cpuinfo | grep -o '[\.0-9]*'", shell=True).split()))/1000, "GHz")
 
     out += " "
     for i in glob("/sys/devices/system/cpu/cpu[0-9]/cpufreq/scaling_governor"):
